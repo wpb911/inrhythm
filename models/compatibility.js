75c1b4947451dd578a669ac6a10bb5e6f73de326
Omega <wpb911@gmail.com>
2021-02-03 23:21:02 -0500

    added field properties and one-to-many relationship code for the tables.

--- models/compatibility.js
+++ models/compatibility.js
@@ -1,11 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
-    var Compatibility = sequelize.define("compatibility", {
-        userID: DataTypes.INTEGER,
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        day: DataTypes.STRING,
-        month: DataTypes.STRING,
-        year: DataTypes.STRING,
+    var Compatibility = sequelize.define("Compatibility", {
+        // userID: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        // },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        day: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        month: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        year: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     });
+
+    Compatibility.associate = (models) => {
+        // We're saying that a Biorhythm should belong to a User
+        // A Biorhythm can't be created without a User due to the foreign key constraint
+        Compatibility.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false,
+          },
+        });
+      };
+
+
     return Compatibility;
   };
