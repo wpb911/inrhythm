aeeaf3aac3a2f912e31dd0ac5561a776a61c1cd8
Omega <wpb911@gmail.com>
2021-02-09 19:10:52 -0500

    jawsdb path added

--- models/index.js
+++ models/index.js
@@ -14,8 +14,8 @@ console.log(`Use environ = ${config.use_env_variable}`);
 // console.log(`MY environ = ${config.my}`);
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env[process.env.JAWSDB_URL], config);
 } else {
   console.log(`No environ variable `);
   //sequelize = new Sequelize(config.database, config.username, config.password, config);
